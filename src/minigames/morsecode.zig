28a683a995d74bac9f460024ed75b3b609795d4a
Raunak Redkar <raunak.redkar@gmail.com>
2024-04-18 12:49:09 +0200

    small additions to morsecode

--- src/minigames/morsecode.zig
+++ src/minigames/morsecode.zig
@@ -6,6 +6,17 @@ const AssetManager = @import("../AssetManager.zig");
 const constants = @import("../constants.zig");
 const input = @import("../input.zig");
 
+// all morse characters are less than 8 long
+// 1 for * , 2 for -, 0 otherwise
+var keystrokes: [constants.max_player_count][8]u8 = [][]u8{.{0}};
+
+fn assigned_pos(id: usize) @Vector(2, i32) {
+    const top_left_x = 120;
+    const top_left_y = 160;
+    const pos = @Vector(2, i32){ @intCast(80 * (id % 4) + top_left_x), @intCast(80 * (id / 4) + top_left_y) };
+    return pos;
+}
+
 pub fn init(sim: *simulation.Simulation) !void {
     // _ = sim;
     // jag tänker att det ska vara ett klassrum, och alla spelare är elever
@@ -14,11 +25,14 @@ pub fn init(sim: *simulation.Simulation) !void {
     // på tavlan har vi den dynamiska morse code tabellen.
     for (0..constants.max_player_count) |id| {
         _ = try sim.world.spawnWith(.{
-            ecs.component.Plr{ .id = id},
+            ecs.component.Plr{ .id = id },
             ecs.component.Txt{ .string = "Player x" },
-            ecs.component.Pos{ .pos = .{@intCast(64 * (id % 4) + 100), @intCast(64 * (id / 4) + 100)} },
+            ecs.component.Pos{ .pos = assigned_pos(id) },
             ecs.component.Mov{ .velocity = ecs.component.Vec2.init(0, 0) },
-            ecs.component.Tex{ .texture_hash = AssetManager.pathHash("assets/kattis.png") },
+            ecs.component.Tex{
+                .texture_hash = AssetManager.pathHash("assets/kattis.png"),
+                .tint = constants.player_colors[id],
+            },
             // animations för spelarna?
         });
     }
