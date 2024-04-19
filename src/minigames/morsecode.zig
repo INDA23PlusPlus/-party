31c6236ae5a0ca539af8d7ab0c5ab5dbf42831b5
Raunak Redkar <raunak.redkar@gmail.com>
2024-04-19 13:59:56 +0200

    keystrokes implemented rougly, havent tested

--- src/minigames/morsecode.zig
+++ src/minigames/morsecode.zig
@@ -8,7 +8,10 @@ const input = @import("../input.zig");
 
 // all morse characters are less than 8 long
 // 1 for * , 2 for -, 0 otherwise
-var keystrokes: [constants.max_player_count][8]u8 = [][]u8{.{0}};
+//var keystrokes: [constants.max_player_count][8]u8 = undefined;
+//std.mem.zero(keystrokes[0..]);
+var keystrokes: [constants.max_player_count][8]u8 = undefined;
+var last_key_index = std.mem.zeroes([constants.max_player_count]u8);
 
 fn assigned_pos(id: usize) @Vector(2, i32) {
     const top_left_x = 120;
@@ -24,6 +27,14 @@ pub fn init(sim: *simulation.Simulation, _: *const input.InputState) !void {
     // de kommer först in i klassrummet genom en och samma rum och sedan står på angett plats
     // typ i rader. Framför varje spelare kan vi lägga dess morsecode keypresses
     // på tavlan har vi den dynamiska morse code tabellen.
+
+    // kan inte zig, fick hårdkoda detta, help me pls
+    for (0..constants.max_player_count) |id| {
+        for (0..8) |j| {
+            keystrokes[id][j] = 0;
+        }
+    }
+
     for (0..constants.max_player_count) |id| {
         _ = try sim.world.spawnWith(.{
             ecs.component.Plr{ .id = id },
@@ -42,9 +53,8 @@ pub fn init(sim: *simulation.Simulation, _: *const input.InputState) !void {
 pub fn update(sim: *simulation.Simulation, inputs: *const input.InputState, arena: std.mem.Allocator) !void {
     _ = arena;
     rl.drawText("This is a new minigame", 64, 8, 32, rl.Color.blue);
-    // try input(&sim.world, inputs);
-    _ = inputs;
-    _ = sim;
+    try inputSystem(&sim.world, inputs);
+    // try wordSystem(&sim.world)
 }
 
 fn inputSystem(world: *ecs.world.World, inputs: *const input.InputState) !void {
@@ -54,13 +64,20 @@ fn inputSystem(world: *ecs.world.World, inputs: *const input.InputState) !void {
         const state = inputs[plr.id];
         if (state.is_connected) {
             if (state.button_a.is_down) {
-                // TODO: add 1 to keystrokes for current player
+                keystrokes[plr.id][last_key_index[plr.id]] = 1;
+                last_key_index[plr.id] += 1;
             }
             else if (state.button_b.is_down) {
-                // TODO: add 2 to keystrokes for current player
+                keystrokes[plr.id][last_key_index[plr.id]] = 2;
+                last_key_index[plr.id] += 1;
             }
         }
     }
 }
 
+fn wordSystem(world: *ecs.world.World) !void {
+    _ = world;
+    // TODO: check if keystrokes array equals a morse character
+    // in which case go to next word if possible, else give player score
+}
 
