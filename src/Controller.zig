c757a3a21a05254b69e571af48f1da621c575db9
AB <ab3.git2@eit.se>
2024-04-29 17:27:44 +0200

    removed outcommented code

--- src/Controller.zig
+++ src/Controller.zig
@@ -64,15 +64,6 @@ fn pollKeyboard1(previous: input.PlayerInputState) input.PlayerInputState {
         .button_a = a,
         .button_b = b,
     };
-    //    state.is_local = true;
-    //    state.is_connected = true;
-    //
-    //    state.button_a.set(rl.isKeyDown(rl.KeyboardKey.key_x), current_tick);
-    //    state.button_b.set(rl.isKeyDown(rl.KeyboardKey.key_z), current_tick);
-    //    state.button_up.set(rl.isKeyDown(rl.KeyboardKey.key_w), current_tick);
-    //    state.button_down.set(rl.isKeyDown(rl.KeyboardKey.key_s), current_tick);
-    //    state.button_left.set(rl.isKeyDown(rl.KeyboardKey.key_a), current_tick);
-    //    state.button_right.set(rl.isKeyDown(rl.KeyboardKey.key_d), current_tick);
 }
 
 fn pollKeyboard2(previous: input.PlayerInputState) input.PlayerInputState {
@@ -84,14 +75,6 @@ fn pollKeyboard2(previous: input.PlayerInputState) input.PlayerInputState {
         .button_a = a,
         .button_b = b,
     };
-    //    state.is_local = true;
-    //    state.is_connected = true;
-    //    state.button_a.set(rl.isKeyDown(rl.KeyboardKey.key_n), current_tick);
-    //    state.button_b.set(rl.isKeyDown(rl.KeyboardKey.key_m), current_tick);
-    //    state.button_up.set(rl.isKeyDown(rl.KeyboardKey.key_i), current_tick);
-    //    state.button_down.set(rl.isKeyDown(rl.KeyboardKey.key_k), current_tick);
-    //    state.button_left.set(rl.isKeyDown(rl.KeyboardKey.key_j), current_tick);
-    //    state.button_right.set(rl.isKeyDown(rl.KeyboardKey.key_l), current_tick);
 }
 
 fn pollGamepad(gamepad: i32, previous: input.PlayerInputState) input.PlayerInputState {
@@ -115,15 +98,6 @@ fn pollGamepad(gamepad: i32, previous: input.PlayerInputState) input.PlayerInput
         .button_a = a,
         .button_b = b,
     };
-
-    //state.is_local = true;
-    //state.is_connected = rl.isGamepadAvailable(gamepad);
-    //state.button_a.set(rl.isGamepadButtonDown(gamepad, rl.GamepadButton.gamepad_button_right_face_down), current_tick);
-    //state.button_b.set(rl.isGamepadButtonDown(gamepad, rl.GamepadButton.gamepad_button_right_face_right), current_tick);
-    //state.button_up.set(rl.isGamepadButtonDown(gamepad, rl.GamepadButton.gamepad_button_left_face_up), current_tick);
-    //state.button_down.set(rl.isGamepadButtonDown(gamepad, rl.GamepadButton.gamepad_button_left_face_down), current_tick);
-    //state.button_left.set(rl.isGamepadButtonDown(gamepad, rl.GamepadButton.gamepad_button_left_face_left), current_tick);
-    //state.button_right.set(rl.isGamepadButtonDown(gamepad, rl.GamepadButton.gamepad_button_left_face_right), current_tick);
 }
 
 // Polls the state of all input devices at the current time.
