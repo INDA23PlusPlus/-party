c2a4a4aa2af719e0bb5c4c8388bec91970bbb929
Elliot Skogsberg <elliot.skogsberg@gmail.com>
2024-05-06 19:41:55 +0200

    add incremental score depletion to scoreboard minigame

--- src/minigames/scoreboard.zig
+++ src/minigames/scoreboard.zig
@@ -13,8 +13,29 @@ const movement = @import("../physics/movement.zig");
 const collision = @import("../physics/collision.zig");
 const constants = @import("../constants.zig");
 
+// Scoreboard:
+// Deplete local score and add to global score
+// Change minigame once all scores are depleted
+
+// TODO:
+// - Deplete the scores in order
+// - Speed up the score animation as it goes on
+// - Allow pressing button to skip score animation
+
+const score_distribution: [constants.max_player_count]u32 = .{ 1000, 800, 600, 500, 150, 100, 75, 250 }; // completely arbitrary score values, open to change
+const score_decrease_speed = 10; // how much the score decreases every tick
+const wait_time_ticks = 3 * constants.ticks_per_second; // time before switching minigame
+const switching_timer_id = 100;
+const score_text_color = 0xFFCC99FF;
+
+fn scoreFromPlacement(placement: u32) u32 {
+    if (placement >= score_distribution.len) return 0; // bad value
+    return score_distribution[placement];
+}
+
 pub fn init(sim: *simulation.Simulation, _: input.Timeline) !void {
-    for (0..8) |id| {
+    for (0..constants.max_player_count) |id| {
+        // spawn player avatar
         _ = try sim.world.spawnWith(.{
             ecs.component.Plr{ .id = @intCast(id) },
             ecs.component.Pos{ .pos = .{ constants.asset_resolution * 4, 16 + (16 + constants.asset_resolution) * @as(i32, @intCast(id)) } },
@@ -24,79 +45,63 @@ pub fn init(sim: *simulation.Simulation, _: input.Timeline) !void {
             },
             ecs.component.Anm{ .animation = Animation.KattisIdle, .interval = 16, .looping = true },
         });
+        // spawn score counter
+        const placement = sim.meta.minigame_placements[id];
+        const score = scoreFromPlacement(placement);
+        _ = try sim.world.spawnWith(.{
+            ecs.component.Pos{ .pos = .{ constants.asset_resolution * 4, 16 + (16 + constants.asset_resolution) * @as(i32, @intCast(id)) } },
+            ecs.component.Ctr{ .count = score, .id = @intCast(id) },
+            ecs.component.Txt{ .color = score_text_color, .string = "???", .font_size = 18, .subpos = .{ 64, 6 } },
+        });
     }
-
-    _ = try sim.world.spawnWith(.{
-        ecs.component.Ctr{ .id = 100, .count = 20 * 60 },
-    });
-    _ = try sim.world.spawnWith(.{
-        ecs.component.Ctr{ .id = 101, .count = 10 * 60 },
-    });
+    // spawn timer, responsible for changing minigame
+    _ = try sim.world.spawnWith(.{ecs.component.Ctr{ .id = switching_timer_id, .count = 3 * constants.ticks_per_second }});
 }
+
 pub fn update(sim: *simulation.Simulation, inputs: input.Timeline, invar: Invariables) !void {
     _ = inputs;
-    var collisions = collision.CollisionQueue.init(invar.arena) catch @panic("could not initialize collision queue");
+    _ = invar;
 
-    movement.update(&sim.world, &collisions, invar.arena) catch @panic("movement system failed");
+    try depleteScores(sim);
+    const scores_depleted = try checkScoresDepleted(sim);
+    if (scores_depleted) {
+        try tickNextGameTimer(sim);
+    }
 
     animator.update(&sim.world);
-    var query = sim.world.query(&.{ecs.component.Ctr}, &.{ ecs.component.Plr, ecs.component.Tex });
-    while (query.next()) |entity| {
+}
+
+fn depleteScores(sim: *simulation.Simulation) !void {
+    var query = sim.world.query(&.{ ecs.component.Ctr, ecs.component.Txt }, &.{});
+    while (query.next()) |_| {
         const ctr = query.get(ecs.component.Ctr) catch unreachable;
-        switch (ctr.id) {
-            100 => {
-                if (ctr.count == 0) {
-                    for (sim.meta.global_score, 0..constants.max_player_count) |score, id| {
-                        std.debug.print("Score {d}: {d}\n", .{ id, score });
-                    }
-                    sim.meta.minigame_id = 4;
-                    return;
-                } else {
-                    ctr.count -= 1;
-                }
-            },
-            101 => {
-                if (ctr.count == 0) {
-                    for (sim.meta.global_score, 0..constants.max_player_count) |score, id| {
-                        std.debug.print("Score {d}: {d}\n", .{ id, score });
-                    }
-                    try updateScore(sim);
-                    sim.world.kill(entity);
-                    return;
-                } else {
-                    ctr.count -= 1;
-                }
-            },
-            else => {},
-        }
+        const txt = query.get(ecs.component.Txt) catch unreachable;
+        const delta = @min(score_decrease_speed, ctr.count);
+        ctr.count -= delta;
+        sim.meta.global_score[ctr.id] += delta;
+        txt.string = rl.textFormat("%d +%d", .{ sim.meta.global_score[ctr.id], ctr.count });
     }
 }
 
-fn updateScore(sim: *simulation.Simulation) !void {
-    for (0..constants.max_player_count) |id| {
-        const mingame_placement = sim.meta.minigame_placements[id];
-        switch (mingame_placement) {
-            1 => {
-                sim.meta.global_score[id] += 10;
-                sim.meta.minigame_placements[id] = undefined;
-            },
-            2 => {
-                sim.meta.global_score[id] += 5;
-                sim.meta.minigame_placements[id] = undefined;
-            },
-            3 => {
-                sim.meta.global_score[id] += 2;
-                sim.meta.minigame_placements[id] = undefined;
-            },
+fn checkScoresDepleted(sim: *simulation.Simulation) !bool {
+    var query = sim.world.query(&.{ ecs.component.Ctr, ecs.component.Txt }, &.{});
+    while (query.next()) |_| {
+        const ctr = query.get(ecs.component.Ctr) catch unreachable;
+        if (ctr.id >= constants.max_player_count) continue;
+        if (ctr.count > 0) return false;
+    }
+    return true;
+}
 
-            0 => {
-                std.debug.print("Hoppsan det var fel\n", .{});
-                sim.meta.minigame_placements[id] = undefined;
-            },
-
-            else => {
-                sim.meta.minigame_placements[id] = undefined;
-            },
+fn tickNextGameTimer(sim: *simulation.Simulation) !void {
+    var query = sim.world.query(&.{ecs.component.Ctr}, &.{ecs.component.Txt});
+    while (query.next()) |_| {
+        const ctr = query.get(ecs.component.Ctr) catch unreachable;
+        if (ctr.id != switching_timer_id) continue;
+        if (ctr.count > 0) {
+            ctr.count -= 1;
+            return;
         }
+        sim.meta.minigame_id = 4;
     }
 }
