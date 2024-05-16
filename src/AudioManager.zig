b34cdfd7bb16ef4a928cdfe42b560c37bc80c138
ILikeSalmon <emilfaalto@gmail.com>
2024-05-16 17:26:51 +0200

    AudioManager is now a struct

--- src/AudioManager.zig
+++ src/AudioManager.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const rl = @import("raylib");
 
+const Self = @This();
 const Allocator = std.mem.Allocator;
 
 const Context = struct {
@@ -30,34 +31,31 @@ const audio_paths = [_][:0]const u8{
     default_audio,
 };
 
+audio_map: AudioHashMap,
+
+pub fn init(alloc: Allocator) !Self {
+    rl.initAudioDevice();
+    var audio_hash_map = AudioHashMap.init(alloc);
+    for (audio_paths) |path| {
+        const key: u8 = @truncate(std.hash.Wyhash.hash(0, path));
+        const sound = rl.loadSound(path);
+        try audio_hash_map.put(key, sound);
+    }
+
+    return .{ .audio_map = audio_hash_map };
+}
+
+pub fn deinit(self: *Self) void {
+    var iter = self.audio_map.valueIterator();
+    while (iter.next()) |sound| {
+        rl.unloadSound(sound.*);
+    }
+    self.audio_map.deinit();
+    rl.closeAudioDevice();
+}
+
 pub fn path_to_key(comptime path: [:0]const u8) u8 {
     comptime {
         return @truncate(std.hash.Wyhash.hash(0, path));
     }
 }
-
-pub const AudioManager = struct {
-    const Self = @This();
-    audio_map: AudioHashMap,
-
-    pub fn init(alloc: Allocator) !AudioManager {
-        rl.initAudioDevice();
-        var audio_hash_map = AudioHashMap.init(alloc);
-        for (audio_paths) |path| {
-            const key: u8 = @truncate(std.hash.Wyhash.hash(0, path));
-            const sound = rl.loadSound(path);
-            try audio_hash_map.put(key, sound);
-        }
-
-        return .{ .audio_map = audio_hash_map };
-    }
-
-    pub fn deinit(self: *Self) void {
-        var iter = self.audio_map.valueIterator();
-        while (iter.next()) |sound| {
-            rl.unloadSound(sound.*);
-        }
-        self.audio_map.deinit();
-        rl.closeAudioDevice();
-    }
-};
